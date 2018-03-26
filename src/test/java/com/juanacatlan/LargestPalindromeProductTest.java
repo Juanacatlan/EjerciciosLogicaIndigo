30cc38e1d7a28b8c53ab905e390981f3449d502e
juanacatlan <juanakarenina24@gmail.com>
2018-03-25 20:26:02 -0600

    first commit

---
+++ src/test/java/com/juanacatlan/LargestPalindromeProductTest.java
@@ -0,0 +1,27 @@
+package com.juanacatlan;
+
+import junit.framework.TestCase;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class LargestPalindromeProductTest extends TestCase {
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void isPalindrome() throws Exception {
+
+    }
+
+    @Test
+    public void main() throws Exception {
+
+    }
+
+}
---
+++ src/test/java/com/juanacatlan/LargestProductInASeriesTest.java
@@ -0,0 +1,25 @@
+package com.juanacatlan;
+
+import junit.framework.TestCase;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static junit.framework.Assert.assertEquals;
+
+public class LargestProductInASeriesTest extends TestCase {
+
+    private LargestProductInASeries product;
+    @Before
+    public void setUp() {
+    }
+
+    @After
+    public void tearDown() {
+    }
+
+    @Test
+    public void main() {
+
+    }
+}
