c4d245df92a3184818a672db17ffa6e826a50a12
Juanacatlan <juanakarenina24@gmail.com>
2018-03-27 18:49:15 -0600

    una prueba que aun no funciona

--- src/main/java/LargestProductInASeries.java
+++ src/main/java/LargestProductInASeries.java
@@ -2,12 +2,7 @@ import java.util.Scanner;
 
 public class LargestProductInASeries {
 
-    private int calculateProduct(int n) {
-        Scanner in = new Scanner(System.in);
-        int k = in.nextInt();
-        String num = in.next();
-
-
+    private int calculateProduct(int n, int k, String num) {
         int largestProduct = 0;
         for (int i = 0; i <= n - k; i++){
             int product = 1;
@@ -16,27 +11,10 @@ public class LargestProductInASeries {
             }
             if (product > largestProduct) largestProduct = product;
         }
-        System.out.println(largestProduct);
-
-        return largestProduct;
-    }
-
-    private int testCases(int t) {
-        Scanner in = new Scanner(System.in);
-        int n = in.nextInt();
-        int largestProduct = 0;
-        for(int a0 = 0; a0 < t; a0++){
-            largestProduct = calculateProduct(n);
-        }
         return largestProduct;
     }
 
     public static void main(String[] args) {
-
-        Scanner in = new Scanner(System.in);
-        int t = in.nextInt();
-
-        new LargestProductInASeries().testCases(t);
-
+        LargestProductInASeries lpp = new LargestProductInASeries();
     }
 }
---
+++ src/test/java/LargestProductInASeriesTest.groovy
@@ -0,0 +1,25 @@
+import spock.lang.Specification
+import spock.lang.Unroll
+import sun.util.resources.cldr.naq.CalendarData_naq_NA
+
+class LargestProductInASeriesTest extends Specification {
+
+    @Unroll('the series of 5 digits in a number that has the largest product')
+    def 'the result product is equals to the number expected'() {
+        given: 'An instace of the class'
+          LargestProductInASeries lpp = new LargestProductInASeriesTest()
+        and: 'the number of test cases, a number of n digits, a size of the series and the number'
+          int n = _n
+          int k = _k
+          String num = _num
+        when: 'compare the product obtained and the expected'
+          int largestProduct = lpp.calculateProduct(n, k, num)
+        then: 'The result must be the expected'
+          largestProduct == _expected
+        where:
+          _n | _k | _num |  _expected
+          10 | 5 | '3675356291' | 3150
+          10 | 5 | '2709360626' | 0
+    }
+}
+
